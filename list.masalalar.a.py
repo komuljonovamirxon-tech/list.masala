118dc688eea3bde3a04b75ac69a8aca7c49b2df6
Amirxon <komuljonovamirxon@gmail.com>
2025-10-31 18:49:13 +0500

    Add files via upload

---
+++ list.masalalar.a.py
@@ -0,0 +1,204 @@
+# fruits = ['olma', 'banan', 'uzum']
+# print(fruits)
+# # 1-vazifa: Ro'yxatga 'nok' ni oxiriga qo'shing (append()).
+# fruits.append('nok')
+# print("1-vazifa: Nok oxiriga qo'shildi", fruits)
+#
+# # 2-vazifa: Ro'yxatning uzunligini toping (len()).
+# length = len(fruits)
+# print("2-vazifa: Ro'yxatning uzunligi:", length)
+#
+# # 3-vazifa: Ro'yxatda 'olma' mavjudligini tekshiring (in operatori).
+# if 'olma' in fruits:
+#     print("3-vazifa: 'olma' ro'yxatda mavjud")
+# else:
+#     print("3-vazifa: 'olma' ro'yxatda mavjud emas")
+#
+# # 4-vazifa: 'banan' ning indeksini toping (index()).
+# print("4-vazifa: 'banan' ning indeksi:", fruits.index('banan'))
+#
+# # 5-vazifa: Ro'yxatni chop eting.
+# print("5-vazifa: Ro'yxat:", fruits)
+
+# #2-masala
+# colors = ['qizil','yashil','kok']
+# print(colors)
+# colors.append('sariq')
+# colors.sort()
+# print(colors)
+# l = len(colors)
+# print(l)
+# if "yashil" in colors:
+#     print("true")
+# else:
+#     print("false")
+# print(colors)
+
+# #3-masala
+# cities = ['Toshkent', 'Samarqand', 'Buxoro']
+# print(cities)
+# cities.append("xiva")
+# print(cities)
+# d = cities.index("Samarqand")
+# print(d)
+# o = len(cities)
+# print(o)
+# cities.sort()
+# print(cities)
+
+# #4-masala
+# numbers = [5, 2, 8, 1]
+# print(numbers)
+# numbers.append(10)
+# numbers.sort()
+# print(numbers)
+# t = len(numbers)
+# print(t)
+# if 8 in numbers:
+#     print("true")
+# else:
+#     print("false")
+# print(numbers)
+
+# #5-masala
+# animals = ['mushuk', 'it', 'qush']
+# print(animals)
+# animals.append("baliq")
+# animals.sort()
+# print(animals)
+# k = animals.index("it")
+# print(k)
+# d = len(animals)
+# print(d)
+# print(animals)
+
+# #6-masala
+# fruits = ['olma', 'banan', 'uzum', 'nok']
+# print(fruits)
+# fruits.append("kiwi")
+# print(fruits)
+# fruits.remove('banan')
+# print(fruits)
+# fruits.insert(2,"shaftoli")
+# print(fruits)
+# fruits.sort()
+# print(fruits)
+# g = len(fruits)
+# print(g)
+# q = fruits.index("olma")
+# print(q)
+# if "kiwi" in fruits:
+#     print("true")
+# else:
+#     print("false")
+# fruits.reverse()
+# print(fruits)
+# v = fruits.copy()
+# print(v)
+# print(fruits)
+
+# #7-masala
+# menu = ['osh', 'lagmon', 'shashlik', 'somsa']
+# print(menu)
+# h = ['manti', 'norin']
+# menu.extend(h)
+# print(menu)
+# menu.remove('lagmon')
+# print(menu)
+# menu.sort()
+# print(menu)
+# menu.pop()
+# print(menu)
+# r = len(menu)
+# print(r)
+# z = menu.index("osh")
+# menu.count("somsa")
+# if 'manti' in menu:
+#     print('true')
+# else:
+#     print('false')
+# e = menu.copy()
+# e.append("qozon kabob")
+# print(e)
+# print(menu)
+
+# #8-masala
+# cities = ['Toshkent', 'Samarqand', 'Buxoro', 'Xiva']
+# print(cities)
+# cities.insert(0, 'andijon')
+# print(cities)
+# cities.remove("Xiva")
+# t = cities.index("Samarqand")
+# print(t)
+# cities.sort()
+# print(cities)
+# y = len(cities)
+# print(y)
+# if "Buxoro" in cities:
+#     print("true")
+# else:
+#     print("false")
+# cities.pop()
+# print(cities)
+# cities.reverse()
+# print(cities)
+# h = cities.copy()
+# h.append("Fargona")
+# print(h)
+# print(cities)
+
+# #9-masala
+# students = ['Ali', 'Vali', 'Sardor', 'Aziz']
+# print(students)
+# students.append("jama")
+# print(students)
+# students.remove("Sardor")
+# print(students)
+# students.insert(1,"diyor")
+# print(students)
+# students.sort()
+# print(students)
+# l = students.index("Aziz")
+# print(l)
+# if "diyor" in students:
+#     print("true")
+# else:
+#     print("false")
+# students.pop()
+# print(students)
+# u = len(students)
+# print(u)
+# b = students.copy()
+# b.append("nodir")
+# print(b)
+# print(students)
+
+# #10-masala
+# favorites = ['telefon', 'noutbuk', 'naushnik']
+# print(favorites)
+# favorites.append("planshet")
+# print(favorites)
+# favorites.append("soat")
+# print(favorites)
+# favorites.sort()
+# print(favorites)
+# k = len(favorites)
+# print(k)
+# cde = favorites.index("noutbuk")
+# print(cde)
+# if "telefon" in favorites:
+#     print("true")
+# else:
+#     print("false")
+# favorites.remove("naushnik")
+# print(favorites)
+# favorites.pop()
+# print(favorites)
+# fd = favorites.copy()
+# fd.append("televizor")
+# fd.reverse()
+# print(fd)
+# favorites.reverse()
+# print(favorites)
+
+
